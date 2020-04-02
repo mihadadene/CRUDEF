506c49f9fa15f33850776117c06d88a5de8fe677
mihadadene <mi.ihadadene@gmail.com>
2020-04-02 00:51:33 -0400

    first commit

---
+++ CRUDEF/CustomValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace CRUDEF
+{
+    public class SkillsValidate : Attribute, IModelValidator
+    {
+        public string[] Allowed { get; set; }
+        public string ErrorMessage { get; set; }
+        public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context)
+        {
+
+            if (Allowed.Contains(context.Model as string))
+                return Enumerable.Empty<ModelValidationResult>();
+            else
+                return new List<ModelValidationResult> {
+                    new ModelValidationResult("", ErrorMessage)
+                };
+        }
+    }
+}
---
+++ CRUDEF/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CRUDEF
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<SchoolContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2); 
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            app.UseDeveloperExceptionPage();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
